481a9049d508a3f0ef86833200ea0ccac194df2c
Ben Ellentuck <ellentuckben@gmail.com>
2018-02-13 21:21:51 -0500

    Adding options of addl bots, realizing chatterbots limitations...

--- table.py
+++ table.py
@@ -23,13 +23,13 @@ def generateMarkovCharacter(textfilePath):
         #yield beginning
         yield markov.chunk_seq(markov_input_text, beginning, 5, 12) + '.'
 
-def processChatterbotRequests():
+def processChatterbotRequests(names):
     bots = []
     n = int(sys.argv[1]) - len(sys.argv) + 2
-    names = ['Margret', 'Sheila', 'Mortimer', 'Oscar', 'Billiph', 'Joseffia']
+    # ['Margret', 'Sheila', 'Mortimer', 'Oscar', 'Billiph', 'Joseffia']
     for i in range(0, n):
         print ('\nTraining ' + names[i])
-        bots.append(chatter.initBot(names[i]))
+        bots.append(chatter.initBot(names[i], 'gossip'))
     return bots
 
 
@@ -45,32 +45,46 @@ def print_title():
 def main():
     print_title()
     markovPlayers = process_markov_player_files()
-
-    chatterPlayers = processChatterbotRequests()
+    names = [ 'Judienne', 'MagentasMaggiesMemory','Elvyn', 'PeculiarPenny', '\'Tunia', 'Centrifuge',
+    'V. C. Pipes', 'RobarackObotama', 'Vixen', 'Vestiges', 'Shangela', 'Brooose', 'Ver de Koons', 'RebeccawithnoZs', 'Talia', 'Grenda', 'Bwana', 'Fin', 'History\'s Greatest Nightmare', 'History\'s Greatest Daydream', 'Vestiges II', 'Reckoning', 'Oranges', 'FamiliarFolly', 'Quine', 'DareDebbie', 'Orman', 'FocusFrancisFocus', 'Tornado-Tuesdae', 'QuackAttack', 'Yelle', 'Merk', 'Phil', 'Grahnt', 'Gebius', 'TokyoTerry', 'Forcefield C. Davis', 'FerdinandoArmando', 'Betty', 'Veronica', 'Once', 'Twice', 'Feathers', 'Heathers', 'McSheathers', 'Daffodeliliah', 'Reece', 'Benjammin', 'JD', 'Death2AllHumanz']
+    chatterPlayers = processChatterbotRequests(names)
     print('\nBots trained. And now...\n\n')
 
+    # n bots
+    convoHasBegun = False
+    for _ in range(0,10):
+        i = 0
+        for bot in chatterPlayers:
+            if (convoHasBegun==False):
+                    line = bot.get_response(next(markovPlayers[0]))
+                    convoHasBegun = True
+            line = str(bot.get_response(line))
+            print(names[i] + ': ' + line)
+            i += 1
+
+
     # # 2 bots and a markov
-    # for _ in range(0,10):
+    # for _ in range(0,20):
     #     markovLine = next(markovPlayers[0])
     #     line = str(chatterPlayers[1].get_response(markovLine))
-    #     print( 'Markov: ' + markovLine)
-    #     print('Alice:  ' + line)
+    #     print( names[2] + ': ' + markovLine)
+    #     print(names[0] + ': ' + line)
     #     line = str(chatterPlayers[0].get_response(line))
-    #     print('Sheila:  ' + line)
+    #     print(names[1] + ': ' + line)
 
     # # 2 bots
     # line = chatterPlayers[1].get_response(next(markovPlayers[0]))
     # for _ in range(0,10):
     #     line = str(chatterPlayers[0].get_response(line))
-    #     print('Alice:  ' + line)
+    #     print(names[0] + ': ' + line)
     #     line = str(chatterPlayers[1].get_response(line))
-    #     print('Sheila:  ' + line)
+    #     print(names[1] + ':  ' + line)
 
-    # single philosophising bot
-    line = str(chatterPlayers[0].get_response(next(markovPlayers[0])))
-    for _ in range(0,10):
-        print('Margret: ' + line)
-        line = str(chatterPlayers[0].get_response(line))
+    # # single philosophising bot
+    # line = str(chatterPlayers[0].get_response(next(markovPlayers[0])))
+    # for _ in range(0,10):
+    #     print('Margret: ' + line)
+    #     line = str(chatterPlayers[0].get_response(line))
 
 
 if __name__ == '__main__':
